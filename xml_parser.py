c798d87f4bfb9cb98104f8fb41814d722138571e
florin_stingaciu <florin_stingaciu@symantec.com>
2016-09-07 17:53:53 +0000

    Update xml_parser.py

--- xml_parser.py
+++ xml_parser.py
@@ -9,19 +9,6 @@ class Parser():
 		striped_xml = XmlnsStriper(raw_xml).stripped
 		root = ElementTree.XML(striped_xml)
 		self.metadata = XmlDictConfig(root)
-def formatXML(parent):
-	ret = {}
-	if parent.items(): ret.update(dict(parent.items()))
-	if parent.text: ret['__content__'] = parent.text
-	if ('List' in parent.tag):
-		ret['__list__'] = []
-		for element in parent:
-			ret['__list__'].append(formatXML(element))
-	else:
-		for element in parent:
-			ret[element.tag] = formatXML(element)
-	return ret
-
 
 class XmlnsStriper():
 	def __init__(self, xml):
